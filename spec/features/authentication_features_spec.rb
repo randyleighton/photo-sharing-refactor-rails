5261dcec828fbe0f62c9e11c98763b8f69304dfb
Randy Leighton & jennifermccarthy <student@epicodus.com>
2014-09-10 15:29:32 -0700

    integration testing for user logging out

--- spec/features/authentication_features_spec.rb
+++ spec/features/authentication_features_spec.rb
@@ -7,31 +7,31 @@ describe "user sign in" do
     fill_in 'Password', with: 'testpassword'
     fill_in 'Password confirmation', with: 'testpassword'
     click_button 'Create User'
-    page.should have_content 'Welcome user@email.com'
+    expect(page).to have_content 'Welcome user@email.com'
   end
 
   it "will take an existing user to the log in page" do
     visit '/signup'
     click_link 'Log In'
-    page.should have_content 'Login'
+    expect(page).to have_content 'Login'
+  end
+
+  it "will login a user who enters credentials successfully" do
+    user = User.create(email: 'user@email.com', password: 'testpassword')
+    visit '/login'
+    fill_in 'Email', with: 'user@email.com'
+    fill_in 'Password', with: 'testpassword'
+    click_button 'Log In'
+    expect(page).to have_content 'Welcome user@email.com'
+  end
+
+  it "will successfully log out a user" do
+    user1 = User.create(email: 'user@email.com', password: 'testpassword')
+    visit '/login'
+    fill_in 'Email', with: 'user@email.com'
+    fill_in 'Password', with: 'testpassword'
+    click_button 'Log In'
+    click_link 'Log Out'
+    expect(page).to have_content 'logged out'
   end
 end
-
-
-
-
-
-
-
-
-
-
-
-
-
-    # user = User.create(email: 'user@email.com', password_digest: 'testpassword')
-#     fill_in 'Email', with: 'user@email.com'
-#     fill_in 'Password', with: 'testpassword'
-#     fill_in 'Password confirmation', with: 'testpassword'
-#     click_button 'Create User'
-# end
